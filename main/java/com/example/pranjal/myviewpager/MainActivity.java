1fcc0067a6954ffefa47edb8aae0aaf67c357d8c
pranjal <pranjalsahu5@gmail.com>
2015-06-16 19:33:40 +0530

    Error happening sometimes

--- main/java/com/example/pranjal/myviewpager/MainActivity.java
+++ main/java/com/example/pranjal/myviewpager/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.pranjal.myviewpager;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterSession;
 
-import io.fabric.sdk.android.Fabric;
 
-
-public class MainActivity  extends BaseActivity {
+public class MainActivity extends BaseActivity {
 
     TwitterSession    currentSession = null;
     TwitterAuthConfig authConfig     = null;
@@ -34,15 +31,15 @@ public class MainActivity  extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView(R.layout.activity_viewpagertab);
-        //setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setContentView(R.layout.activity_viewpagertab);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-
+        /*
         String TWITTER_KEY    = "i8lsarVzM1RLdQli7JvGibJya";
          String TWITTER_SECRET = "ivA141Pewjx3VYfKOUBMIRJZZnNhPQNW9gVdM1nlXrnsNmir29";
 
         authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));//
+        Fabric.with(this, new Twitter(authConfig));
 
         MyApplication appState = ((MyApplication) getApplicationContext());
         currentSession         = Twitter.getSessionManager().getActiveSession();
@@ -59,6 +56,6 @@ public class MainActivity  extends BaseActivity {
             Intent intent = new Intent(MainActivity.this, ViewPagerTabListViewActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             appState.startActivity(intent);
-        }
+        }*/
     }
 }
